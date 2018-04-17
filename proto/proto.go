0acf2893505e7f930fe940b3c86e38ad134380e3
chenjunhan <1471827997@qq.com>
2018-04-17 23:05:47 +0800

    add push proto

--- proto/proto.go
+++ proto/proto.go
@@ -1,5 +1,9 @@
 package proto
 
+import (
+	"encoding/json"
+)
+
 type TokenVerifyData struct {
 	Token string `string:"token"`
 	Uid   string `json:"uid"`
@@ -10,8 +14,51 @@ func (t *TokenVerifyData) Valid() bool {
 	return len(t.Uid) != 0
 }
 
+// Session Sp session struct
 type Session struct {
 	SessionID string `json:"id"`
 	UID       string `json:"uid"`
 	Name      string `json:"name`
+}
+
+const (
+	NotifyLabel  = "code"
+	NotifyNone   = "0"
+	NotifyLogout = "1"
+)
+
+// Push message define
+const (
+	Error = 0
+	Ok    = 1
+	// from idp to cluster, must litter than TypeCommas
+	IdpLogout = 1
+
+	TypeCommas = 255
+
+	// frome cluster to idp, must bigger than TypeCommas
+	ClusterLogout      = 256
+	ClusterVerifyToken = 257
+)
+
+// PushMsg push message struct
+type PushMsg struct {
+	Type    int    `json:"type"`
+	Content string `json:"content"`
+}
+
+func (m *PushMsg) String() string {
+	s, _ := json.Marshal(*m)
+
+	return string(s)
+}
+
+func ToPushMsg(msg []byte) PushMsg {
+	pm := PushMsg{
+		Type: Error,
+	}
+
+	json.Unmarshal(msg, &pm)
+
+	return pm
 }
